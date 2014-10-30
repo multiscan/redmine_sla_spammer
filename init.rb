f476f316b81fa09448fcead27e22d448f42f1f4f
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2014-10-30 16:00:15 +0100

    Just after rails generate redmine_plugin SlaSpammer

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :sla_spammer do
+  name 'Sla Spammer plugin'
+  author 'Giovanni Cangiani - Kandou Bus SA'
+  description 'Adds e-mail reminders for issues to be assigned and completed'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
