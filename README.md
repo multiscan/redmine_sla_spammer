abcf463a347087dd8860c68a666b45b2b0bf980b
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2014-10-30 16:08:48 +0100

    Minimal read me for github.

---
+++ README.md
@@ -0,0 +1,8 @@
+Redmine SLA spammer
+===================
+
+This is a simple plugin for [Redmine](http://www.redmine.org) that will deliver
+e-mail messages to developer/managers in order to push them to respect deadlines.
+
+It was written for [Kandou Bus](http://www.kandou.com/) internal use, but feel
+free to use/extend it as you like.
--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :sla_spammer do
   author 'Giovanni Cangiani - Kandou Bus SA'
   description 'Adds e-mail reminders for issues to be assigned and completed'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/multiscan/redmine_sla_spammer'
+  author_url 'http://algo.epfl.ch/en/group/members/giovanni'
 end
